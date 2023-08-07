93742483d3ad28dba70efc456729c860601923ae
lec2528 <132907542+lec2528@users.noreply.github.com>
2023-08-07 22:46:15 +0900

    feat:.env 파일 및 환경변수 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,29 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { typeORMConfig } from './configs/typeorm.config'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config.service';
 import { BoardsModule } from './Boards/boards.module';
 import { CardsModule } from './Cards/cards.module';
-import { CommentsModule } from './Comments/comments.module'
-import { ListsModule } from './Lists/lists.module'
-import { MembersModule } from './Members/members.module'
-import { UsersModule } from './Users/users.module'
-
+import { CommentsModule } from './Comments/comments.module';
+import { ListsModule } from './Lists/lists.module';
+import { MembersModule } from './Members/members.module';
+import { UsersModule } from './Users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {} from './configs/typeorm.config.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeORMConfig),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: typeORMConfig,
+      inject: [ConfigService],
+    }),
     BoardsModule,
     CardsModule,
     CommentsModule,
     ListsModule,
     MembersModule,
-    UsersModule
+    UsersModule,
   ],
 })
 export class AppModule {}
