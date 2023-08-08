37203895cf2aa594674ca9b66b8ee5cabfc14052
lec2528 <132907542+lec2528@users.noreply.github.com>
2023-08-08 15:27:20 +0900

    fix:DB연결이 안되어서 커밋 되돌림

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from './configs/typeorm.config.service';
+import { typeORMConfig } from './configs/typeorm.config';
 import { BoardsModule } from './Boards/boards.module';
 import { CardsModule } from './Cards/cards.module';
 import { CommentsModule } from './Comments/comments.module';
 import { ListsModule } from './Lists/lists.module';
 import { MembersModule } from './Members/members.module';
 import { UsersModule } from './Users/users.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import {} from './configs/typeorm.config.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useClass: typeORMConfig,
-      inject: [ConfigService],
-    }),
+    TypeOrmModule.forRoot(typeORMConfig),
     BoardsModule,
     CardsModule,
     CommentsModule,
---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: '',
+  port: 3000,
+  username: '',
+  password: '',
+  database: '',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
