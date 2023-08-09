596b4c5bdba4f4365a9b908fef0715a8293912ea
lec2528 <132907542+lec2528@users.noreply.github.com>
2023-08-09 16:05:04 +0900

    feat:board CRUD 구현 업로드

--- src/Boards/boards.controller.ts
+++ src/Boards/boards.controller.ts
@@ -1,6 +1,47 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, ValidationPipe } from '@nestjs/common';
-import { Boards } from "./boards.entity"
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Boards } from './boards.entity';
+import { BoardsService } from './boards.service';
 
+@Controller('api')
+export class BoardsController {
+  constructor(private readonly boardService: BoardsService) {}
 
-@Controller('Boards')
-export class BoardsController {}
+  @Get('/board/:bid')
+  getBoard(@Param('bid') bid: number) {
+    return this.boardService.getBoard(bid);
+  }
+
+  @Post('/board')
+  createBoard(@Body() data: Boards) {
+    return this.boardService.createBoard(
+      data.name,
+      data.color,
+      data.explanation,
+    );
+  }
+
+  @Patch('/board/:bid')
+  updateBoard(@Param('bid') bid: number, @Body() data: Boards) {
+    return this.boardService.updateBoard(
+      bid,
+      data.name,
+      data.explanation,
+      data.color,
+    );
+  }
+
+  @Delete('board/:bid')
+  deleteBoard(@Param('bid') bid: number) {
+    return this.boardService.deleteBoard(bid);
+  }
+}
--- src/Boards/boards.service.ts
+++ src/Boards/boards.service.ts
@@ -1,7 +1,44 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Boards } from './boards.entity';
 
 @Injectable()
-export class BoardsService{
+export class BoardsService {
+  constructor(
+    @InjectRepository(Boards)
+    private boardRepository: Repository<Boards>,
+  ) {}
 
+  async getBoard(bid: number) {
+    if (!bid) {
+      throw new NotFoundException('존재하지 않는 보드 입니다.');
+    }
+    return await this.boardRepository.findOne({ where: { bid } });
+  }
+  createBoard(name: string, color: string, explanation: string) {
+    return this.boardRepository.insert({ name, color, explanation });
+  }
+  async updateBoard(
+    bid: number,
+    name: string,
+    color: string,
+    explanation: string,
+  ) {
+    await this.checkBoard(bid);
+    this.boardRepository.update(bid, { name, color, explanation });
+  }
+
+  async deleteBoard(bid: number) {
+    this.boardRepository.delete(bid);
+  }
+  //보드가 존재하는지 확인하는 함수 작성
+  private async checkBoard(bid: number) {
+    const board = await this.boardRepository.findOne({
+      where: { bid },
+    });
+    if (!board) {
+      throw new NotFoundException(`${bid}보드는 존재하지 않는 보드 입니다.`);
+    }
+  }
 }
